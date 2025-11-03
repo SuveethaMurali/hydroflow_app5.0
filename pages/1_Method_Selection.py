bf6e5e4a9f281827afee7cca6eb8cb4f2a513da1
Suveetha M <suveethamurali10@gmail.com>
2025-11-03 13:44:52 +0530

    Update 1_Method_Selection.py

--- pages/1_Method_Selection.py
+++ pages/1_Method_Selection.py
@@ -2,12 +2,12 @@ import streamlit as st
 
 st.title("Method Selection")
 
-method = st.radio("Select a method:", ["SCN Method", "Strangers Method"])
+method = st.radio("Select a method:", ["SCN Method", "Strange's Method"])
 
 if st.button("Continue ➡️"):
     if method == "SCN Method":
         st.session_state.method = "scn"
         st.switch_page("pages/2_SCN_Method.py")
     else:
-        st.session_state.method = "strangers"
-        st.switch_page("pages/3_Strangers_Method.py")
+        st.session_state.method = "strange's"
+        st.switch_page("pages/3_Strange's_Method.py")
